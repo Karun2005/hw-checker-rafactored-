24278f0076c3b304f6d21c7c88b867e28e1289b5
ArvindhSengu <117862704+ArvindhSengu@users.noreply.github.com>
2022-11-15 14:38:30 -0500

    Create MarksClient.java

---
+++ MarksClient.java
@@ -0,0 +1,12 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+import java.util.Arrays;
+import java.io.*;
+
+class MarksClient {
+  public static void main(String[] args) {
+ 
+ }
+}
