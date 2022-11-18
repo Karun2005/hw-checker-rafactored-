9e8e4b57305da155181a99c737527da3f47f49c9
ArvindhSengu <117862704+ArvindhSengu@users.noreply.github.com>
2022-11-18 11:35:19 -0500

    Update MarksClient.java
    Removed all unnecessary code and moved it to a new class called Validate

--- MarksClient.java
+++ MarksClient.java
@@ -1,49 +1,8 @@
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.Scanner;
-import java.util.Arrays;
-import java.io.*;
-import java.nio.file.Files;
-import java.nio.file.LinkOption;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-
-
 class MarksClient {
   public static void main(String[] args) {
-    boolean useEquationSover = validateStringInput();
-    if(useEquationSover){
-      System.out.println("Using EQ Solver");
-    }
-    else{
-      System.out.println("Not equation solver");
-    }
-    userInput();
-  }
-    
-    
-    
-    
-   public static boolean validateStringInput(){
-    System.out.println("Welcome to Student Marking AI V5.3");
-    System.out.println("Select one of the options below to continue");
-    System.out.println("A. Import answer data and mark student data");
-    System.out.println("B. Run equation solver and mark student data");
-    Scanner reader = new Scanner(System.in);
-    String choice = reader.nextLine();
-
-    while(!choice.equalsIgnoreCase("A") && !choice.equalsIgnoreCase("B")){
-      System.out.println("Invalid resonse, try again.");
-      choice = reader.nextLine();
-    }
-
-    if(choice.equalsIgnoreCase("B")){
-      return true;
-    }
-    else if(choice.equalsIgnoreCase("A")){
-      return false;
-    }
-    return true;
+    ValidateString validater = new ValidateString();
+    MarksFactory marksFactory = new MarksFactory();
+    Marks marks1 = marksFactory.getMarks(validater.stringInput());
+    marks1.exportMarks();
   }
 }
