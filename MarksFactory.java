48349c6e94ac810a42adc660f3ebeb43b5f2cc0c
Karun2005 <117862707+Karun2005@users.noreply.github.com>
2022-11-21 21:26:08 -0500

    Update MarksFactory.java
    comments for marks factory

--- MarksFactory.java
+++ MarksFactory.java
@@ -1,3 +1,4 @@
+//based on if user chose to compare using the answer file or solving the questions, the designated action will be taken to complete the task
 class MarksFactory {
     public Marks getMarks(boolean useEquationSolver){
         if(useEquationSolver){
