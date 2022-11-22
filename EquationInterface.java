250de6962db5bd45463ae0ef4ea3fa3e94861808
Karun2005 <117862707+Karun2005@users.noreply.github.com>
2022-11-21 21:29:06 -0500

    Update EquationInterface.java
    Comments for equation Interface

--- EquationInterface.java
+++ EquationInterface.java
@@ -1,3 +1,4 @@
+//cleans the question array so that it can be accessed to solve for the values with no trouble 
 interface EquationInterface {
     public String[] cleanEquations(String[][] questionData);
     public String[] solveEquation(String[] questionArray);
