540df482129f377c9e5ab8b83fa7bb9644be49b6
Karun2005 <117862707+Karun2005@users.noreply.github.com>
2022-11-21 21:22:48 -0500

    Update WithSolver.java
    comments for class that sole equations

--- WithSolver.java
+++ WithSolver.java
@@ -1,3 +1,5 @@
+//class that runs if user chooses to compare the student response and answer by solving equation
+//In our code, class will not be run since it cannot solve the equations
 class WithSolver extends DataMethods implements Marks{
 
     @Override
